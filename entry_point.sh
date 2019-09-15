ba95f6c2a459a67a953132044dc6002fea56196c
汪啸 <wangxiao33@meituan.com>
2019-09-15 23:16:51 +0800

    change cert suffix to cer as per acme script

--- entry_point.sh
+++ entry_point.sh
@@ -20,7 +20,7 @@ if [ "$1" = 'aria2c' -a "$(id -u)" = '0' ]; then
         echo "enabling ssl on rpc connection"
         exec su-exec $UID:$GID aria2c --conf-path="/app/conf/aria2.conf" \
             --enable-rpc --rpc-listen-all \
-            --rpc-certificate=/app/conf/key/aria2.crt \
+            --rpc-certificate=/app/conf/key/aria2.cer \
             --rpc-private-key=/app/conf/key/aria2.key \
             --rpc-secret="$RPC_SECRET" --rpc-secure \
             "$@"
